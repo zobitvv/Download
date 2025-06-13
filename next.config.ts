3095a1499e875daedfe682881e7a08122d305838
Zohaib 43 <zobitech76@gmail.com>
2025-06-13 04:14:33 +0000

    https://image.winudf.com/v2/image1/Y29tLm51bWJlcmZpbmRlci5wYWtfc2ltX2Rhd

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'image.winudf.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
